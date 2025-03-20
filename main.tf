59fafb0cdb397840a1101c7f261590437b89ffc8
devops-tsj <github.tsj@gmail.com>
2025-03-20 18:48:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tsj-backend-tfstates"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    
+  }
+}
+
+
 provider "aws" {
   region = var.region
 }
